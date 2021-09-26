b85c0c141ddd57c02e84bd6302b201140c8e5963
Milton D. Yama <miltondyama@gmail.com>
2021-09-25 23:18:23 -0400

    desafio-7

---
+++ Desafio7/Classes/Conta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Desafio7.Classes
+{
+    public class Conta
+    {
+        public Conta(string titular, string numero, decimal saldo)
+        {
+            Titular = titular;
+            Numero = numero;
+            Saldo = saldo;
+        }
+
+        public string Titular { get; private set; }
+        public string Numero { get; private set; }
+        private decimal Saldo { get; set; }
+
+        public void VerificarSaldo()
+        {
+            Console.WriteLine($"Seu saldo atual é de {Saldo} reais");
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= 0 || valor > Saldo)
+            {
+                Console.WriteLine("Verifique o valor informado e tente novamente");
+            }
+            else
+            {
+                Saldo -= valor;
+                Console.WriteLine($"Você sacou {valor} reais");
+            }
+
+            VerificarSaldo();
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("Informe um valor válido para depositar");
+            }
+            else
+            {
+                Saldo += valor;
+                Console.WriteLine($"Você depositou {valor} reais");
+            }
+
+            VerificarSaldo();
+        }
+    }
+}
---
+++ Desafio7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Desafio7.Classes;
+
+namespace Desafio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta novaConta = new Conta("Milton Daniel Yama", "0540562", 50m);
+            novaConta.VerificarSaldo();
+
+            novaConta.Sacar(30m);
+            novaConta.Depositar(80m);
+
+            Console.ReadKey();
+        }
+    }
+}
