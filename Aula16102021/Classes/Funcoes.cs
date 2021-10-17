23475c996147e2ea597d49ad3fcb362919cf3c74
Milton D. Yama <miltondyama@gmail.com>
2021-10-17 12:27:30 -0400

    Aula 16.10.2021

---
+++ Aula16102021/Classes/Funcoes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula16102021.Classes
+{
+    public class Funcoes
+    {
+        public void BuscarNumeroPar(int numero, int posicaoInicial)
+        {
+            for (int i = 0; i < posicaoInicial; i++)
+            {
+                Console.WriteLine(numero + i * 2);
+            }
+        }
+    }
+}
---
+++ Aula16102021/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Aula16102021.Classes;
+
+namespace Aula16102021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para descobrir a sequência de dez pares: ");
+            var numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite quantas posições você quer: ");
+            var posicaoInicial = Convert.ToInt32(Console.ReadLine());
+
+            Funcoes funcoes = new Funcoes();
+            funcoes.BuscarNumeroPar(numero, posicaoInicial);
+
+            Console.ReadLine();
+        }
+    }
+}
