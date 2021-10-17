c9d7167daec8d469020c6376302756707c879a9c
Milton D. Yama <miltondyama@gmail.com>
2021-10-17 12:28:31 -0400

    Desafio 9

---
+++ Desafio9/Classes/Calculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Desafio9.Classes
+{
+    public class Calculo
+    {
+        public void SequenciaFibonacci(int limiteDaSequencia)
+        {
+            int numeroAnterior;
+            int numeroAtual = 0;
+            int numeroPosterior = 1;
+
+            Console.Write($"{numeroAtual}, {numeroPosterior}, ");
+
+            for (int i = 0; i < limiteDaSequencia; i++)
+            {
+                numeroAnterior = numeroAtual;
+                numeroAtual = numeroPosterior;
+                numeroPosterior = numeroAtual + numeroAnterior;
+
+                Console.Write($"{numeroPosterior}, ");
+            }
+        }
+    }
+}
---
+++ Desafio9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Desafio9.Classes;
+
+namespace Desafio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o limite da sequência de fibonacci: ");
+            int limiteDaSequencia = Convert.ToInt32(Console.ReadLine());
+
+            Calculo calculo = new Calculo();
+            calculo.SequenciaFibonacci(limiteDaSequencia);
+
+            Console.ReadLine();
+        }
+    }
+}
